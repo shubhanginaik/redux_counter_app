13b9c38745a5f7942a032ece59737a0f6ac48e5c
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-03-11 21:19:53 +0200

    added color change on odd and even counter

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import classes from './Circle.module.css';
+import ids from './Circle.module.css';
 
 import {useSelector} from 'react-redux';
     const Circle=()=>{
         const counter= useSelector((state)=>state.counter);
-        return <div className={classes.circle}>{counter}</div>
+        
+        if(counter===0){
+            return <div className={classes.circle}>{counter}</div>
+        }
+        else if(counter%2!==0){
+            return <div className={classes.circle} id={ids.odd}>{counter}</div>
+        }
+        else{
+            return <div className={classes.circle} id={ids.even}>{counter}</div>
+        }
     }
     export default Circle;
